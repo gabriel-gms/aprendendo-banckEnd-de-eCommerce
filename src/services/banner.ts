6590525dd8b57119fa87c538671f3068c54d6836
Gabriel Gomes <gggabriel368@gmail.com>
2026-01-12 12:25:24 -0300

    terminando a rota de banners

--- src/services/banner.ts
+++ src/services/banner.ts
@@ -1,5 +1,11 @@
-import { prisma } from "../libs/prisma"
+import {prisma} from "../libs/prisma"
 
 export const getAllBanners = async () => {
-    const banners = await prisma
+    const banners = await prisma.banner.findMany({
+        select: { img: true, link: true }
+    })
+    return banners.map(banner => ({
+        ...banner,
+        img: `media/banners/${banner.img}`
+    }))
 }
